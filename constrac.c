776dd6f9969eea11e6465bdbed4f792eb365142a
Ayoub <hassankoba1999@gmail.com>
2023-08-23 15:15:30 -0700

    com

--- constrac.c
+++ constrac.c
@@ -105,7 +105,7 @@ void constrac_unsetenv(data *data)
 	(void)data;
 	if (!data->av[1] || !_getv(data->av[1]))
 	{
-		_perror(data->shell_nm, "variable not found.");
+		perror(data->shell_nm, "variable not found.");
 		return;
 	}
 	len = strlen(data->av[1]);
